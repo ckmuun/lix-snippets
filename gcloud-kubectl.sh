f58db9edb9eea63408cb5f8156784c82201d5e2e
Cornelius Koller <55432690+ckmuun@users.noreply.github.com>
2021-01-12 12:07:56 +0000

    simple oneliner for setting kubectl to find a gke cluster

---
+++ gcloud-kubectl.sh
@@ -0,0 +1,5 @@
+# set kubectl context for newly created GKE cluster
+export PROJECT_ID = "insert your project id here"
+export GKE_CLUSTER_ID = "insert your gke cluster id here"
+
+gcloud container clusters get-credentials $GKE_CLUSTER_ID --region europe-west3 --project $PROJECT_ID
