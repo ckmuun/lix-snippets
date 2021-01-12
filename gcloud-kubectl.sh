e1c39ef189e833f9fe9574ff93bb0e2f64a4a6a7
Cornelius Koller <55432690+ckmuun@users.noreply.github.com>
2021-01-12 12:12:52 +0000

    Update gcloud-kubectl.sh

--- gcloud-kubectl.sh
+++ gcloud-kubectl.sh
@@ -5,4 +5,5 @@ export GKE_CLUSTER_ID = "insert your gke cluster id here"
 echo 'checking gcloud version if it fails, probably gcloud cli not installed'
 gcloud version
 
+echo 'setting up kubectl to use gke cluster $GKE_CLUSTER_ID'
 gcloud container clusters get-credentials $GKE_CLUSTER_ID --region europe-west3 --project $PROJECT_ID
